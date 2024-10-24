99989a834a3c82ed16f7052f3a60e4037b63ce06
timvisee <tim@visee.me>
2024-10-24 09:47:24 +0200

    Also configure separate snapshot directories in run.sh

--- run.sh
+++ run.sh
@@ -4,9 +4,10 @@ set -e
 
 QDRANT_BIN=${QDRANT_BIN:-./target/debug/qdrant}
 STORAGE_BASE=/tmp/storage_
+SNAPSHOTS_BASE=/tmp/snapshots_
 
 if [[ $1 == "rm" ]]; then
-    rm -rf $STORAGE_BASE*
+    rm -rf $STORAGE_BASE* $SNAPSHOTS_BASE*
     echo "All storages removed!"
     exit 0
 fi
@@ -19,6 +20,7 @@ shift
 export QDRANT__CLUSTER__ENABLED=true
 export QDRANT__SERVICE__HOST=$IP
 export QDRANT__STORAGE__STORAGE_PATH=$STORAGE_BASE$ID
+export QDRANT__STORAGE__SNAPSHOTS_PATH=$SNAPSHOTS_BASE$ID
 
 if [[ $ID == 1 ]]; then
     $QDRANT_BIN --uri http://127.0.0.1:6335 $@
